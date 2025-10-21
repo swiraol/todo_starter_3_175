7bf8e39a2d074b453c87ff883af2db99dd38f506
Robert Tsui <rt.roberttsui@gmail.com>
2025-10-21 12:23:26 -0700

    added new files

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is a todo starter!</h1>"
+    lists = [
+        {"title": "Lunch Groceries", "todos": ["Buy milk", "Buy sugar"]},
+        {"title": "Dinner Groceries", "todos": ["Buy chicken", "Buy beef"]}        
+    ]
+    return render_template('lists.html', lists=lists)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
