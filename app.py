3465b3a92fe389dc0f64160693cf3d0d9fb7139c
Robert Tsui <rt.roberttsui@gmail.com>
2025-10-22 17:06:41 -0700

    Add a new todo list (part 1)

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return redirect(url_for('get_lists'))
+
+@app.route('/lists', methods=['GET', 'POST'])
+def get_lists():
     lists = [
-        {"title": "Lunch Groceries", "todos": ["Buy milk", "Buy sugar"]},
-        {"title": "Dinner Groceries", "todos": ["Buy chicken", "Buy beef"]}        
+        {'title': 'Lunch Groceries', 'todos': []},
+        {'title': 'Dinner Groceries', 'todos': []},
     ]
     return render_template('lists.html', lists=lists)
 
 @app.route("/lists/new")
-def new_list():
+def add_new_list():
     return render_template('new_list.html')
 
 if __name__ == "__main__":
