dc89c223f9f12c62aff95a4b030df54bc54f68ff
Robert Tsui <rt.roberttsui@gmail.com>
2025-11-03 15:50:09 -0800

    Added a post route for adding a todo to the session object

--- todos/utils.py
+++ todos/utils.py
@@ -7,5 +7,11 @@ def error_for_list_title(list_title, todo_lists):
     
     return None
 
+def error_for_todo_title(todo_title):
+    if not 1 <= len(todo_title) <= 100:
+        return 'The title must be between 1 and 100 characters.'
+    
+    return None
+
 def find_list_by_id(list_id, lists):
     return next((lst for lst in lists if list_id == lst['id']), None)
